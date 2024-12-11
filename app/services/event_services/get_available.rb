1b6eff3335d55f60cd541790f826d5c159c2e490
Stephanie Encina <stephjose2011@gmail.com>
2024-12-12 00:41:05 +0800

    Additional comments

--- app/services/event_services/get_available.rb
+++ app/services/event_services/get_available.rb
@@ -15,12 +15,15 @@ module EventServices
       my_schedule = CalendarEntity.new(user_id)
       their_schedule = CalendarEntity.new(available_with)
 
+      ## Now let's load the events/schedules to the user's calendar
       my_schedule = update_schedules(user_id, my_schedule)
       their_schedule = update_schedules(available_with, their_schedule)
 
-      ## Now let's compare schedule of university vs my schedule
+      ## Now let's compare schedules
+      ## This will return an array list of event keys
       available_schedule = ScheduleComparattor.compare(their_schedule, my_schedule)
 
+      # We'll now retrieve the event details for each of the available / non-conflicting schedule
       list = []
       available_schedule.each_with_index do |key, index|
         list << their_schedule.events[key.to_i].event_details
@@ -31,7 +34,6 @@ module EventServices
 
     def update_schedules(user_id, calendar)
       Event.where(user_id: user_id).each do |event|
-        # datetime = DateTime.strptime(@value, '%Y-%m-%d')
         calendar.block_schedule(event.name, event.start, event.end)
       end
 
