3fae57f1840920257c244df2bfe926c8630804b7
Stephanie Encina <stephjose2011@gmail.com>
2024-12-12 01:08:35 +0800

    Remove unused attribute

--- app/entities/event_entity.rb
+++ app/entities/event_entity.rb
@@ -8,24 +8,11 @@ class EventEntity
     @end_date = end_date
   end
 
-  def book
-    @booked = true
-  end
-
-  def unbook
-    @booked = false
-  end
-
-  def booked?
-    booked === true
-  end
-
   def event_details
     {
       start_date: start_date,
       end_date: end_date,
-      name: name,
-      booked: booked?
+      name: name
     }
   end
 end
